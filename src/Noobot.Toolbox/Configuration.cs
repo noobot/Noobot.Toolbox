f60b94c5bf34904030843c197339fb09dfbcb601
Simon C <simon_colmer@hotmail.com>
2016-01-18 09:05:56 +0000

    Added comment around the standard configuration

--- src/Noobot.Toolbox/Configuration.cs
+++ src/Noobot.Toolbox/Configuration.cs
@@ -4,6 +4,9 @@ using Noobot.Toolbox.Plugins;
 
 namespace Noobot.Toolbox
 {
+    /// <summary>
+    /// A standard configuration with all of the middleware & plugins found in the toolbox. Note: This is an example and probably shouldn't be used in production code.
+    /// </summary>
     public class Configuration : ConfigurationBase
     {
         public Configuration()
