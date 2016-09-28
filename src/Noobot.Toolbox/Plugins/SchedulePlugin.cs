0bd5687e859bd0d211d07f86248e723136a23273
Simon C <simon_colmer@hotmail.com>
2016-09-28 16:48:26 +0100

    Moved code for scheduling a task to ScheduledJob

--- src/Noobot.Toolbox/Plugins/SchedulePlugin.cs
+++ src/Noobot.Toolbox/Plugins/SchedulePlugin.cs
@@ -1,43 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Common.Logging;
-using Noobot.Core;
-using Noobot.Core.Logging;
-using Noobot.Core.MessagingPipeline.Response;
 using Noobot.Core.Plugins;
 using Noobot.Core.Plugins.StandardPlugins;
+using Noobot.Toolbox.Plugins.Scheduling;
 using Quartz;
 using Quartz.Impl;
-using SlackConnector.Models;
+using Quartz.Impl.AdoJobStore;
 
 namespace Noobot.Toolbox.Plugins
 {
     public class SchedulePlugin : IPlugin
     {
         private string FileName { get; } = "schedules";
+        private readonly StructuremapJobFactory _jobFactory;
         private readonly JsonStoragePlugin _storagePlugin;
-        private readonly INoobotCore _noobotCore;
         private readonly StatsPlugin _statsPlugin;
-        private readonly ILog _log;
         private readonly object _lock = new object();
         private readonly List<ScheduleEntry> _schedules = new List<ScheduleEntry>();
+        private IScheduler _scheduler;
 
-        public SchedulePlugin(JsonStoragePlugin storagePlugin, INoobotCore noobotCore, StatsPlugin statsPlugin, ILog log)
+        public SchedulePlugin(StructuremapJobFactory jobFactory, JsonStoragePlugin storagePlugin, StatsPlugin statsPlugin)
         {
+            _jobFactory = jobFactory;
             _storagePlugin = storagePlugin;
-            _noobotCore = noobotCore;
             _statsPlugin = statsPlugin;
-            _log = log;
         }
 
         public void Start()
         {
             lock (_lock)
             {
-                ScheduleEntry[] schedules = _storagePlugin.ReadFile<ScheduleEntry>(FileName);
-                _schedules.AddRange(schedules);
+                _scheduler = StdSchedulerFactory.GetDefaultScheduler();
+                _scheduler.JobFactory = _jobFactory;
 
+                ScheduleEntry[] schedules = _storagePlugin.ReadFile<ScheduleEntry>(FileName);
+                foreach (var schedule in schedules)
+                {
+                    AddSchedule(schedule);
+                }
+
+                _scheduler.Start();
             }
         }
 
@@ -48,10 +51,18 @@ namespace Noobot.Toolbox.Plugins
 
         public void AddSchedule(ScheduleEntry schedule)
         {
+            if (!CronExpression.IsValidExpression(schedule.CronSchedule))
+            {
+                _statsPlugin.IncrementState("schedules:invalidcron");
+                throw new InvalidConfigurationException("Cron expression is invalid");
+            }
+
             lock (_lock)
             {
+                schedule.Created = DateTime.Now;
                 ExecuteSchedule(schedule);
                 _schedules.Add(schedule);
+                _statsPlugin.IncrementState("schedules:added");
             }
 
             Save();
@@ -63,8 +74,7 @@ namespace Noobot.Toolbox.Plugins
             {
                 ScheduleEntry[] schedules = _schedules
                                                 .Where(x => x.Channel == channel)
-                                                .OrderBy(x => x.LastRun)
-                                                .ThenBy(x => x.Command)
+                                                .OrderBy(x => x.Command)
                                                 .ToArray();
                 return schedules;
             }
@@ -75,106 +85,48 @@ namespace Noobot.Toolbox.Plugins
             lock (_lock)
             {
                 ScheduleEntry[] schedules = _schedules
-                                                .OrderBy(x => x.LastRun)
-                                                .ThenBy(x => x.Command)
+                                                .OrderBy(x => x.Command)
                                                 .ToArray();
                 return schedules;
             }
         }
 
-        public void DeleteSchedules(ScheduleEntry[] scheduleEntries)
+        public void DeleteSchedules(Guid[] guids)
         {
             lock (_lock)
             {
-                foreach (ScheduleEntry scheduleEntry in scheduleEntries)
+                foreach (Guid guid in guids)
                 {
-                    _schedules.Remove(scheduleEntry);
+                    _scheduler.UnscheduleJob(new TriggerKey(guid.ToString(), "job"));
+
+                    ScheduleEntry toRemove = _schedules.First(x => x.Guid == guid);
+                    _schedules.Remove(toRemove);
+                    _statsPlugin.IncrementState("schedules:deleted");
                 }
             }
 
             Save();
         }
 
-        public void DeleteSchedule(ScheduleEntry scheduleEntry)
+        public void DeleteSchedule(Guid guid)
         {
-            DeleteSchedules(new[] { scheduleEntry });
+            DeleteSchedules(new[] { guid });
         }
 
         private void ExecuteSchedule(ScheduleEntry schedule)
         {
-            var job = JobBuilder.Create();
-            job.UsingJobData("guid", schedule.Guid.ToString());
-           // job.
+            IJobDetail job = JobBuilder.Create<ScheduledJob>()
+                            .UsingJobData("guid", schedule.Guid.ToString())
+                            .WithIdentity(schedule.Guid.ToString())
+                            .Build();
 
-
-            var trigger = TriggerBuilder.Create()
-                .WithIdentity("trigger3", "group1")
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity(new TriggerKey(schedule.Guid.ToString(), "job"))
                 .WithCronSchedule(schedule.CronSchedule)
-                .ForJob(job.Build())
                 .Build();
 
-            IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
-            //scheduler.JobFactory = new 
-            //scheduler.
+            _scheduler.ScheduleJob(job, trigger);
         }
-
-        // private void RunSchedules(object sender, ElapsedEventArgs e)
-        //{
-        //     lock (_lock)
-        //     {
-        //         _statsPlugin.RecordStat("Schedules:LastRun", DateTime.Now.ToString("G"));
-        //         _statsPlugin.RecordStat("Schedules:IsCurrentlyNight", IsCurrentlyNight().ToString());
-
-        //         foreach (var schedule in _schedules)
-        //         {
-        //             ExecuteSchedule(schedule);
-        //         }
-        //     }
-
-        //     Save();
-        // }
-
-        //private void ExecuteSchedule(ScheduleEntry schedule)
-        //{
-        //    if (ShouldRunSchedule(schedule))
-        //    {
-        //        _log.Log($"Running schedule: {schedule}");
-
-        //        SlackChatHubType channelType = schedule.ChannelType == ResponseType.Channel
-        //            ? SlackChatHubType.Channel
-        //            : SlackChatHubType.DM;
-
-        //        var slackMessage = new SlackMessage
-        //        {
-        //            Text = schedule.Command,
-        //            User = new SlackUser { Id = schedule.UserId, Name = schedule.UserName },
-        //            ChatHub = new SlackChatHub { Id = schedule.Channel, Type = channelType },
-        //        };
-
-        //        _noobotCore.MessageReceived(slackMessage);
-        //        schedule.LastRun = DateTime.Now;
-        //    }
-        //}
-
-        //private static bool ShouldRunSchedule(ScheduleEntry schedule)
-        //{
-        //    bool shouldRun = false;
-        //    if (!schedule.LastRun.HasValue)
-        //    {
-        //        shouldRun = true;
-        //    }
-        //    else if (schedule.LastRun + schedule.RunEvery < DateTime.Now)
-        //    {
-        //        shouldRun = true;
-        //    }
-
-        //    if (shouldRun & schedule.RunOnlyAtNight)
-        //    {
-        //        shouldRun = IsCurrentlyNight();
-        //    }
-
-        //    return shouldRun;
-        //}
 
         private void Save()
         {
@@ -182,23 +134,6 @@ namespace Noobot.Toolbox.Plugins
             {
                 _storagePlugin.SaveFile(FileName, _schedules.ToArray());
                 _statsPlugin.RecordStat("Schedules:Active", _schedules.Count);
-            }
-        }
-
-        public class ScheduleEntry
-        {
-            public Guid Guid { get; set; }
-            public DateTime? LastRun { get; set; }
-            public string CronSchedule { get; set; }
-            public string Command { get; set; }
-            public string Channel { get; set; }
-            public ResponseType ChannelType { get; set; }
-            public string UserId { get; set; }
-            public string UserName { get; set; }
-
-            public override string ToString()
-            {
-                return $"Running command `'{Command}'` every `'{CronSchedule}'`. Last run at `'{LastRun}'`. Guid {Guid}";
             }
         }
     }
